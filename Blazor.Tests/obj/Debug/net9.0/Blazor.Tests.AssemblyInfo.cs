0fd44af550b9012aade08a6a8191cb4494f69ff4
Berdan-Y <b.yilmaz7@student.avans.nl>
2025-06-22 22:38:12 +0200

    Create TokenService test

--- Blazor.Tests/obj/Debug/net9.0/Blazor.Tests.AssemblyInfo.cs
+++ Blazor.Tests/obj/Debug/net9.0/Blazor.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d5d840e98ae661118fcd62206c6cad9150a91b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed71fbf325cbcf97cef0fa85bcdf1e38c062c7e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
